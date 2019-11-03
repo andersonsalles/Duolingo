0ec1e14d161c8e7b6551ea5c6dc93b608b57d2f5
Anderson <andersonsalles@outlook.com>
2019-11-03 20:27:29 -0300

    Adiconando custom render no ios para aumentar o tamanho da tabbed page no ios

---
+++ Duolingo.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Duolingo.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CustomTabbedPageRenderer))]  
+namespace Duolingo.iOS.Renderers
+{
+    class CustomTabbedPageRenderer : TabbedRenderer
+    {
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            var tabFrame  = TabBar.Frame;
+            var viewFrame = View.Frame;
+
+            var tabHeight = 150;
+            tabFrame.Height = tabHeight;
+            tabFrame.Y = View.Frame.Height - 150;
+            View.Frame = viewFrame;
+        }
+    }
+}
