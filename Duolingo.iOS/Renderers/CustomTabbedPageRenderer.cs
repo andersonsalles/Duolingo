e21e7e12716d0f495f90e25bd030c1771d7e46cd
Anderson <andersonsalles@outlook.com>
2019-11-03 21:53:27 -0300

    Customizar iscones abas ios

--- Duolingo.iOS/Renderers/CustomTabbedPageRenderer.cs
+++ Duolingo.iOS/Renderers/CustomTabbedPageRenderer.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Duolingo.iOS.Renderers;
+using Duolingo.Views;
 using Foundation;
 using UIKit;
 using Xamarin.Forms;
@@ -23,6 +25,67 @@ namespace Duolingo.iOS.Renderers
             tabFrame.Height = tabHeight;
             tabFrame.Y = View.Frame.Height - 150;
             View.Frame = viewFrame;
+        }
+
+        protected override async Task<Tuple<UIImage, UIImage>> GetIcon(Page page)
+        {
+            if (page is LessonsView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_lessons.png"),
+                        GetImageFromFile("tab_lessons_selected.png")
+                     )
+                );
+            }
+
+            if (page is TrainingView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_training.png"),
+                        GetImageFromFile("tab_training_selected.png")
+                     )
+                );
+            }
+
+            if (page is ProfileView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_profile.png"),
+                        GetImageFromFile("tab_profile_selected.png")
+                     )
+                );
+            }
+
+            if (page is RankingView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_ranking.png"),
+                        GetImageFromFile("tab_ranking_selected.png")
+                     )
+                );
+            }
+
+            if (page is StoreView)
+            {
+                return await Task.FromResult(
+                    new Tuple<UIImage, UIImage>(
+                        GetImageFromFile("tab_store.png"),
+                        GetImageFromFile("tab_store_selected.png")
+                     )
+                );
+            }
+            return await base.GetIcon(page);
+        }
+
+        private UIImage GetImageFromFile(string fileName)
+        {
+            return UIImage
+                .FromFile(fileName)
+                .ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
         }
     }
 }
