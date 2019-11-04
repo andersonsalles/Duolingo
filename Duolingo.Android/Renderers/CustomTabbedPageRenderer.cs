dd04a54b48eafcb026796d5f2610b801d8f6c2c3
Anderson <andersonsalles@outlook.com>
2019-11-03 22:36:35 -0300

    adicionando lógica dos icones pro andoird

--- Duolingo.Android/Renderers/CustomTabbedPageRenderer.cs
+++ Duolingo.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -12,6 +12,9 @@ namespace Duolingo.Droid.Renderers
 {
     class CustomTabbedPageRenderer: TabbedPageRenderer
     {
+        private TabbedPage _formsTabs;
+        private BottomNavigationView _bottomNavigationView;
+
         public CustomTabbedPageRenderer(Context context) : base(context)
         {
 
@@ -22,61 +25,71 @@ namespace Duolingo.Droid.Renderers
             base.OnElementChanged(e);
             if (e.NewElement != null)
             {
-                var formsTabs = Element;
+                _formsTabs = Element;
+                _formsTabs.CurrentPageChanged += FormsTabs_CurrentPageChanged;
                 var relativeLayout = base.GetChildAt(0) as Android.Widget.RelativeLayout;
-                var bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
-                bottomNavigationView.SetMinimumHeight(350);
-                bottomNavigationView.ItemIconTintList = null;
-                bottomNavigationView.ItemIconSize = 150;
-
-                for (int index = 0; index < formsTabs.Children.Count; index++)
-                {
-                    var androidTab = bottomNavigationView.Menu.GetItem(index);
-
-                    if (formsTabs.Children[index] is LessonsView)
-                    {
-                        if (formsTabs.Children[index] == formsTabs.CurrentPage)
-                        {
-                            androidTab.SetIcon(Resource.Drawable.tab_lessons_selected);
-                            continue;
-                        }
-                        androidTab.SetIcon(Resource.Drawable.tab_lessons);
-                        continue;
-                    }
-                    if (formsTabs.Children[index] is ProfileView)
-                    {
-                        if (formsTabs.Children[index] == formsTabs.CurrentPage)
-                        {
-                            androidTab.SetIcon(Resource.Drawable.tab_profile_selected);
-                            continue;
-                        }
-                        androidTab.SetIcon(Resource.Drawable.tab_profile);
-                        continue;
-                    }
-                    if (formsTabs.Children[index] is RankingView)
-                    {
-                        if (formsTabs.Children[index] == formsTabs.CurrentPage)
-                        {
-                            androidTab.SetIcon(Resource.Drawable.tab_ranking_selected);
-                            continue;
-                        }
-
-                        androidTab.SetIcon(Resource.Drawable.tab_ranking);
-                        continue;
-                    }
-                    if (formsTabs.Children[index] is StoreView)
-                    {
-                        if (formsTabs.Children[index] == formsTabs.CurrentPage)
-                        {
-                            androidTab.SetIcon(Resource.Drawable.tab_store_selected);
-                            continue;
-                        }
-                        androidTab.SetIcon(Resource.Drawable.tab_store);
-                        continue;
-                    }
-
-                }
+                _bottomNavigationView = relativeLayout.GetChildAt(1) as BottomNavigationView;
+                _bottomNavigationView.SetMinimumHeight(350);
+                _bottomNavigationView.ItemIconTintList = null;
+                _bottomNavigationView.ItemIconSize = 150;
+                UpdateAllTabs(_formsTabs, _bottomNavigationView);
             }
+        }
+
+        private void UpdateAllTabs(TabbedPage formsTabs, BottomNavigationView bottomNavigationView)
+        {
+            for (int index = 0; index < formsTabs.Children.Count; index++)
+            {
+                var androidTab = bottomNavigationView.Menu.GetItem(index);
+
+                if (formsTabs.Children[index] is LessonsView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_lessons_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_lessons);
+                    continue;
+                }
+                if (formsTabs.Children[index] is ProfileView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_profile_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_profile);
+                    continue;
+                }
+                if (formsTabs.Children[index] is RankingView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_ranking_selected);
+                        continue;
+                    }
+
+                    androidTab.SetIcon(Resource.Drawable.tab_ranking);
+                    continue;
+                }
+                if (formsTabs.Children[index] is StoreView)
+                {
+                    if (formsTabs.Children[index] == formsTabs.CurrentPage)
+                    {
+                        androidTab.SetIcon(Resource.Drawable.tab_store_selected);
+                        continue;
+                    }
+                    androidTab.SetIcon(Resource.Drawable.tab_store);
+                    continue;
+                }
+
+            }
+        }
+
+        private void FormsTabs_CurrentPageChanged(object sender, EventArgs e)
+        {
+            UpdateAllTabs(_formsTabs, _bottomNavigationView);
         }
     }
 }
